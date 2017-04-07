f3b249cff3a2aeed94a8b5baaed0d3abc9c3f1c5
hidu <duv123+github@gmail.com>
2017-04-07 13:45:49 +0800

    bdlog_filter

---
+++ bdlog_filter/bdlog_filter.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var conds = flag.String("cond", "", "filter condition,eg:   a>0")
+
+var version = "0.1 20170407"
+
+type CondItem struct {
+	Key string
+	Op  string
+	Val float64
+}
+
+func init() {
+	ua := flag.Usage
+	flag.Usage = func() {
+		ua()
+		fmt.Println("\ncat a.log.wf|bdlog_filter")
+		fmt.Println("\n site: https://github.com/hidu/tool")
+		fmt.Println(" version:", version)
+	}
+}
+
+var cond *CondItem
+
+func main() {
+	flag.Parse()
+	if *conds == "" {
+		fmt.Fprint(os.Stderr, "filter is empty\n")
+		os.Exit(1)
+	}
+	var err error
+	cond, err = parseConds(*conds)
+	if err != nil {
+		fmt.Fprint(os.Stderr, "parse filter failed:"+err.Error()+"\n")
+		os.Exit(1)
+	}
+	parselog(os.Stdin)
+}
+
+func parseConds(condStr string) (*CondItem, error) {
+	r := regexp.MustCompile(`(\w+)([><=]+)(\d+(.\d+)?)`)
+	m := r.FindStringSubmatch(condStr)
+	//["a>=4.0" "a" ">=" "4.0" ".0"]
+	if len(m) < 3 {
+		return nil, fmt.Errorf("parse cond failed")
+	}
+	result := &CondItem{
+		Key: strings.TrimSpace(m[1]),
+		Op:  strings.TrimSpace(m[2]),
+	}
+	v, err := strconv.ParseFloat(m[3], 64)
+	if err != nil {
+		return nil, fmt.Errorf("parse cond val failed:%s", err.Error())
+	}
+	result.Val = v
+	return result, nil
+}
+
+func parselog(rd io.Reader) {
+	buf := bufio.NewReaderSize(rd, 81920)
+	for {
+		line, err := buf.ReadBytes('\n')
+		if len(line) > 0 {
+			printLine(line)
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+}
+
+func printLine(line []byte) {
+	kv := parseLine(line)
+	if kv == nil {
+		log.Print("parse line failed:", string(line))
+		return
+	}
+	v_f, _ := strconv.ParseFloat(kv.get(cond.Key), 64)
+	match := false
+	switch cond.Op {
+	case ">":
+		match = v_f > cond.Val
+		break
+	case ">=":
+		match = v_f >= cond.Val
+		break
+	case "<":
+		match = v_f < cond.Val
+		break
+	case "<=":
+		match = v_f <= cond.Val
+		break
+	case "=":
+		match = v_f == cond.Val
+		break
+	default:
+		log.Println("not support operate:", cond.Op)
+		break
+	}
+
+	if match {
+		fmt.Print(string(line))
+	}
+}
+
+type logKv map[string]string
+
+func (kv logKv) get(key string) string {
+	if val, has := kv[key]; has {
+		return val
+	}
+	return ""
+}
+
+var startKey = []byte("[")
+var stopKey = []byte("]")
+var kvSpace = []byte(" ")
+var eqSign = []byte("=")
+
+func parseLine(line []byte) logKv {
+	m := bytes.LastIndex(line, startKey)
+	if m < 1 {
+		return nil
+	}
+	n := bytes.LastIndex(line, stopKey)
+	if n < m {
+		return nil
+	}
+	kv := make(logKv)
+
+	sub := line[m+1 : n]
+	items := bytes.Split(sub, kvSpace)
+
+	for _, item := range items {
+		x := bytes.Index(item, eqSign)
+		if x < 1 {
+			continue
+		}
+		key := string(item[:x])
+		val := string(item[x+1:])
+		kv[key] = val
+	}
+
+	return kv
+}
--- build.sh
+++ build.sh
@@ -11,5 +11,5 @@ go build -o dest/bin/xml2json_ser -ldflags "-s -w"  xml2json_ser/xml2json_ser.go
 go build -o dest/bin/httptest -ldflags "-s -w"  httptest/httptest.go 
 go build -o dest/bin/urldecode -ldflags "-s -w"  urldecode/urldecode.go 
 go build -o dest/bin/bdlog_kv -ldflags "-s -w"  bdlog_kv/bdlog_kv.go 
-go build -o dest/bin/httpserver4test -ldflags "-s -w"  httpserver4test/httpserver4test.go 
 go build -o dest/bin/url_call_conc -ldflags "-s -w"  url_call_conc/url_call_conc.go
+go build -o dest/bin/bdlog_filter -ldflags "-s -w"  bdlog_kv/bdlog_filter.go 
